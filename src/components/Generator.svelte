80b2b38859a94db5f65e32740b8dededcc8b4b45
yekta <ngyekta@gmail.com>
2020-08-09 01:42:56 +0300

    Generator related additions

--- src/components/Generator.svelte
+++ src/components/Generator.svelte
@@ -1,5 +1,32 @@
 <script>
+  import axios from "axios";
   export let generatorVisibility = false;
+  let inputValue;
+  let receivedMonkey = false;
+  let monkeyLoading = false;
+  let showAgainButton = false;
+  let monkeyContainer;
+  let getMonkey = async () => {
+    try {
+      return axios.get(
+        "https://natricon.com/api/v1/nano?svc=natricon.com&address=" +
+          inputValue
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  let generateMonkey = async () => {
+    monkeyLoading = true;
+    let monkeyResult = await getMonkey();
+    if (monkeyResult.data) {
+      receivedMonkey = true;
+      console.log(monkeyResult);
+      monkeyContainer.innerHTML = monkeyResult.data;
+      monkeyLoading = false;
+      showAgainButton = true;
+    }
+  };
 </script>
 
 <style>
@@ -40,20 +67,30 @@
 <div
   class="generator {!generatorVisibility ? 'closed' : ''} border-primary flex
   flex-col bg-white absolute top-0 mt-5">
-  <div class="w-full h-full flex flex-col px-4 md:px-8 py-4 md:py-5">
+  <div bind:this={monkeyContainer} />
+  <!-- Input, Show Me & Randomize -->
+  <div
+    class="{monkeyLoading || receivedMonkey ? 'scale-0' : 'scale-100'} transform
+    duration-200 ease-out w-full h-full flex flex-col px-4 md:px-8 py-4 md:py-5">
     <div class="flex flex-col items-center my-auto">
       <input
+        bind:value={inputValue}
         class="w-full text-xl font-bold px-4 py-2 border-2 border-black
         rounded-xl"
         type="text"
         placeholder="enter your address" />
       <button
+        disabled={monkeyLoading || receivedMonkey}
+        on:click={() => {
+          generateMonkey();
+        }}
         class="w-full bg-primary btn-primary text-white text-xl font-bold
         rounded-xl border-2 border-black px-6 py-2 mx-auto mt-3">
         Show Me
       </button>
     </div>
     <button
+      disabled={monkeyLoading || receivedMonkey}
       class="bg-primary btn-primary text-white text-lg font-bold rounded-lg
       border-2 border-black px-6 md:px-8 py-1 mx-auto">
       Randomize
