62b26208dae71f6835a90a39cf274d910a5581fd
yekta <ngyekta@gmail.com>
2020-08-28 19:09:41 +0300

    New lazy load

--- src/components/LazyImage.svelte
+++ src/components/LazyImage.svelte
@@ -1,91 +1,48 @@
 <script>
-  export let offset = 100;
-  export let onload = null;
+  export let offset = 200;
   export let classes;
-  let loaded = false;
-  function load(node) {
-    const loadHandler = throttle((e) => {
-      const top = node.getBoundingClientRect().top;
-      const expectedTop = getExpectedTop(e, offset);
-      if (top <= expectedTop) {
-        loaded = true;
-        onload && onload(node);
-        removeListeners();
-      }
-    }, 200);
-    loadHandler();
-    addListeners();
-    function addListeners() {
-      document.addEventListener("scroll", loadHandler, true);
-      window.addEventListener("resize", loadHandler);
+  import { onMount } from "svelte";
+  let observer = null;
+  let showContent = false;
+  let item;
+  function onIntersection(entries) {
+    if (entries[0].isIntersecting && !showContent) {
+      showContent = true;
     }
-    function removeListeners() {
-      document.removeEventListener("scroll", loadHandler, true);
-      window.removeEventListener("resize", loadHandler);
-    }
+  }
+  function lazyLoad(node) {
+    observer && observer.observe(node);
     return {
-      destroy: () => {
-        removeListeners();
+      destroy() {
+        observer && observer.unobserve(node);
       },
     };
   }
-  function getExpectedTop(e, offset) {
-    const height = getContainerHeight(e);
-    return height + offset;
-  }
-  function getContainerHeight(e) {
-    if (e && e.target && e.target.getBoundingClientRect) {
-      return e.target.getBoundingClientRect().bottom;
-    } else {
-      return window.innerHeight;
+  onMount(async () => {
+    let hasAPI = "IntersectionObserver" in window;
+    if (hasAPI) {
+      observer = new IntersectionObserver(onIntersection, { rootMargin: `${offset}px` });
+      lazyLoad(item);
     }
-  }
-  function throttle(func, wait, options) {
-    let context, args, result;
-    let timeout = null;
-    let previous = 0;
-    if (!options) options = {};
-    const later = function () {
-      previous = options.leading === false ? 0 : new Date();
-      timeout = null;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    };
-    return function (event) {
-      const now = new Date();
-      if (!previous && options.leading === false) previous = now;
-      const remaining = wait - (now - previous);
-      context = this;
-      args = arguments;
-      if (remaining <= 0 || remaining > wait) {
-        if (timeout) {
-          clearTimeout(timeout);
-          timeout = null;
-        }
-        previous = now;
-        result = func.apply(context, args);
-        if (!timeout) context = args = null;
-      } else if (!timeout && options.trailing !== false) {
-        timeout = setTimeout(later, remaining);
-      }
-      return result;
-    };
-  }
+  });
 </script>
 
-<div class={classes}>
-  <div use:load class="w-full h-full relative">
+<div bind:this={item} class={classes}>
+  <div class="w-full h-full relative">
     <div
-      class="{loaded ? 'opacity-0' : 'opacity-100'} w-full h-full transition-opacity duration-500"
+      class="{showContent ? 'opacity-0' : 'opacity-100'} w-full h-full transition-opacity
+      duration-500"
     >
       <slot name="placeholder" />
     </div>
     <div
-      class="{loaded ? 'opacity-100' : 'opacity-0'} w-full h-full transition-opacity duration-300
-      ease-out absolute top-0 left-0"
+      class="{showContent ? 'opacity-100' : 'opacity-0'} w-full h-full transition-opacity
+      duration-300 ease-out absolute top-0 left-0"
     >
-      <slot name="content" />
-      <slot />
+      {#if showContent}
+        <slot name="content" />
+        <slot />
+      {/if}
     </div>
   </div>
 </div>
