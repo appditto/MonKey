3ff17f7ddc561c29629c665619e355529da33398
Brandon Berhent <bberhent@ascend-innovations.com>
2022-08-29 18:13:48 -0400

    Fix dependabot merge

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import svelte from "rollup-plugin-svelte";
 import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import config from "sapper/config/rollup.js";
-import json from 'rollup-plugin-json';
+import json from "rollup-plugin-json";
 
 import pkg from "./package.json";
 
